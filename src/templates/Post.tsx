1f41ed521df94aaf4032231fdb4f0ce518fce88e
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2022-02-05 16:43:22 +0500

    🎨 STYLES: Seperate CSS styles

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { slugs, node } from '@/types';
 
 import { Avatar, Button, Grid, Paper, Stack, Typography } from '@mui/material';
 import { deepPurple } from '@mui/material/colors';
+import '../styles/templateStyles.css'
 
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
 import { BLOCKS } from '@contentful/rich-text-types';
@@ -34,30 +34,30 @@ const Post: React.FC<PostContextProps> = ({ pageContext }) => {
 
                 <Grid item xs={12}>
 
-                    <Paper style={{ padding: '3vw 4vw 3vw 4vw' }}>
+                    <Paper className='paper-styles'>
 
                         <Stack direction='row' spacing={0}>
                             <Avatar sx={{ bgcolor: deepPurple[500] }}>{pageContext.data.node.author.substring(0, 1)}</Avatar>
-                            <Typography variant='body2' style={{ marginLeft: '8px' }}>
+                            <Typography variant='body2' className='author-name'>
                                 {pageContext.data.node.author}<br />
                                 <Typography variant='caption'>{pageContext.data.node.createdAt}</Typography>
                             </Typography>
                         </Stack>
 
-                        <Typography style={{ fontSize: '6vw' }}>{pageContext.data.node.title}</Typography>
+                        <Typography className='article-title'>{pageContext.data.node.title}</Typography>
 
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '5px' }}>
-                            <img src={pageContext.data.node.image.file.url} style={{ width: '100%', maxHeight: '60%', borderRadius: '20px', marginTop: '5px' }} />
+                        <div className='image-container'>
+                            <img src={pageContext.data.node.image.file.url} className='image' />
                         </div>
 
                         <Typography>{
                             renderRichText(pageContext.data.node.content, {
                                 renderNode: {
                                     [BLOCKS.HEADING_1]: (_, children) => (
-                                        <Typography variant='h1' style={{ fontSize: '6vw' }}><b>{children}</b></Typography>
+                                        <Typography variant='h1' className='heading'><b>{children}</b></Typography>
                                     ),
                                     [BLOCKS.PARAGRAPH]: (_, children) => (
-                                        <Typography variant='body1' style={{ marginTop: '7px', marginBottom: '7px' }}>{children}</Typography>
+                                        <Typography variant='body1' className='paragraph'>{children}</Typography>
                                     ),
                                     [BLOCKS.QUOTE]: (_, children) => (
                                         <Typography variant='body2'><blockquote>{children}</blockquote></Typography>
@@ -68,16 +68,16 @@ const Post: React.FC<PostContextProps> = ({ pageContext }) => {
 
                         {
                             pageContext.data.node.content?.references ?
-                                pageContext.data.node.content?.references?.map((item, i) => {
-                                    return <div style={{ display: 'flex', justifyContent: 'center', margin: '5px' }} key={i}>
-                                        <img src={item.file.url} style={{ borderRadius: '20px' }} />
+                                pageContext.data.node.content?.references?.map((item: References, i) => {
+                                    return <div className='article-image-container' key={i}>
+                                        <img src={item.file.url} className='article-image' />
                                     </div>
                                 })
                                 : null
                         }
 
-                        <div style={{ justifyContent: 'center', display: 'flex' }}>
-                            <Button><Link to={`/post/${pageContext.nextPageSlugs[randomIndexNumber].node.slug}`} style={{ textDecoration: 'none', color: '#000000' }}>
+                        <div className='button-container'>
+                            <Button><Link to={`/post/${pageContext.nextPageSlugs[randomIndexNumber].node.slug}`} className='link'>
                                 Next
                             </Link></Button>
                         </div>
